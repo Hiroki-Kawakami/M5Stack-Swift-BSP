7d83c799fa4a5c3eb36a698e34ed6af090492d70
Hiroki Kawakami <40638139+Hiroki-Kawakami@users.noreply.github.com>
2026-01-03 16:13:04 +0900

    FFI Wrapper Retain API

--- FFI/FFI.swift
+++ FFI/FFI.swift
@@ -2,5 +2,18 @@ enum FFI {
     class Wrapper<T> {
         let value: T
         init(_ value: T) { self.value = value }
+
+        static func unretained(_ opaquePointer: UnsafeMutableRawPointer!) -> T {
+            Unmanaged<Self>.fromOpaque(opaquePointer!).takeUnretainedValue().value
+        }
+        static func retained(_ opaquePointer: UnsafeMutableRawPointer!) -> T {
+            Unmanaged<Self>.fromOpaque(opaquePointer!).takeRetainedValue().value
+        }
+        func passUnretained() -> UnsafeMutableRawPointer! {
+            Unmanaged.passUnretained(self).toOpaque()
+        }
+        func passRetained() -> UnsafeMutableRawPointer! {
+            Unmanaged.passRetained(self).toOpaque()
+        }
     }
 }
